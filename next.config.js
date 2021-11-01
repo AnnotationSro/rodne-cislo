9d2b62bed3eb44e624e3bbd1fa310f57637d22fe
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2021-11-01 20:30:42 +0100

    ignore linting errors during production build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  // Warning: This allows production builds to successfully complete even if
-  // your project has ESLint errors.
-  ignoreDuringBuilds: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
