d3fe4c8427184d8838464fbd21a8171c91487be2
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2021-11-02 09:12:31 +0100

    page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import "../utils/font-awesome";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Generátor rodného čísla</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
